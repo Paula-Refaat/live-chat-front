79d3df7cee6a0b5f30dcc525ba6f7bebaab7b5c2
Paula <poularefaat86@gmail.com>
2024-06-06 01:45:22 +0300

    🔥 remove unused codes

--- src/screens/DashboardScreen.js
+++ src/screens/DashboardScreen.js
@@ -1,35 +1,23 @@
-import React, { useEffect } from "react";
+import React from "react";
+import { useEffect } from "react";
 import { v1 as uuid } from "uuid";
-import useAuthentication from "../hooks/useAuthentication";
-
 import M from "materialize-css";
 
 const DashboardScreen = (props) => {
-  let isAuthenticated = useAuthentication();
   const createRoom = () => {
     const id = uuid();
     props.history.push(`/room/${id}`);
   };
 
   useEffect(() => {
-    if (!isAuthenticated) {
-      M.toast({ html: "Login first", classes: "red" });
-      props.history.push("/login");
-    }
-    //eslint-disable-next-line
-  }, [isAuthenticated]);
-
-  const logout = () => {
-    localStorage.removeItem("Token");
-    props.history.push("/login");
-  };
+    M.AutoInit(); // Initialize Materialize components
+  }, []);
 
   return (
     <div className="container">
       <div className="card-container">
         <div className="card">
           <button onClick={createRoom}>Create room</button>
-          <button onClick={logout}>Logout</button>
         </div>
       </div>
     </div>
